f73d931748f5df9044ce34fb1bf555673879fd88
Charlie Gough <tothemuun21@gmail.com>
2024-02-06 11:13:12 +0000

    Signed-off-by: Charlie Gough <tothemuun21@gmail.com>

---
+++ main.py
@@ -0,0 +1,18 @@
+
+        import pygame, json, os, sys
+
+        from pyredengine import *
+        from pyredengine import SceneService as SceneService
+
+        class Main(App):
+            def __init__(self):
+                super().__init__()
+            
+            def load_scenes(self):
+                # Read Docs 
+                pass
+                
+        if __name__ == "__main__":
+            main = Main()
+            main.run()
+    
